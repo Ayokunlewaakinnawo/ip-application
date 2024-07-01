06009b76fcfe8e8bac607062eaeb635098398248
Ayo Akinnawo <ayo@ayos-mbp-2.vtc.voxtechnologies.com>
2024-07-01 09:13:09 -0500

    Initial commit

--- industrialpartner_app/urls.py
+++ industrialpartner_app/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('industrialpartner.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Serve media files during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#   urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
