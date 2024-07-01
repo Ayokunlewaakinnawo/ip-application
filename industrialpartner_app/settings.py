5c9f17bfc2de8210e38830b84a23beccdaac0b63
Ayo Akinnawo <ayo@Ayos-MBP-2.lan>
2024-06-30 23:04:30 -0500

    Initial commit

--- industrialpartner_app/settings.py
+++ industrialpartner_app/settings.py
@@ -136,5 +136,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '/industrialpartner/static'),
 ]
